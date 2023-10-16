74d4972d56f3f9308efb334bda8c4e55f01c8173
Douglas Lima <douglas.lima.queiroz@gmail.com>
2023-10-16 14:35:39 -0300

    update-table

--- front-end-estatico/Aula-03/exer/index.js
+++ front-end-estatico/Aula-03/exer/index.js
@@ -1,53 +1,67 @@
 document.getElementById('myForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-  
-    const firstname = document.getElementById('firstname').value;
-    const lastname = document.getElementById('lastname').value;
-    const email = document.getElementById('email').value;
-  
-    const apiUrl = 'https://crudcrud.com/api/4ce1796f97ba4e6b9428eccbd7f7de8a/inscritos';
-  
-    const data = {
-      firstname,
-      lastname,
-      email
-    };
-  
-    fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
+  event.preventDefault();
+
+  const firstname = document.getElementById('firstname').value;
+  const lastname = document.getElementById('lastname').value;
+  const email = document.getElementById('email').value;
+  const cargo = document.getElementById('cargo').value; // Adicionado
+  const salario = document.getElementById('salario').value; // Adicionado
+
+  const apiUrl = 'https://crudcrud.com/api/4ce1796f97ba4e6b9428eccbd7f7de8a/inscritos';
+
+  const data = {
+    firstname,
+    lastname,
+    email,
+    cargo, // Adicionado
+    salario // Adicionado
+  };
+
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => {
+      const tableResult = document.getElementById('table').getElementsByTagName('tbody')[0];
+      const newRow = tableResult.insertRow(tableResult.rows.length);
+
+      const rowFirstname = newRow.insertCell(0);
+      rowFirstname.innerHTML = data.firstname;
+
+      const rowLastname = newRow.insertCell(1);
+      rowLastname.innerHTML = data.lastname;
+
+      const rowEmail = newRow.insertCell(2);
+      rowEmail.innerHTML = data.email;
+
+      // Adicionados novos campos na tabela
+      const rowCargo = newRow.insertCell(3);
+      rowCargo.innerHTML = data.cargo;
+
+      const rowSalario = newRow.insertCell(4);
+      rowSalario.innerHTML = data.salario;
+
+      cleanInput();
     })
-      .then(response => response.json())
-      .then(data => {
-        const tableResult = document.getElementById('table').getElementsByTagName('tbody')[0];
-        const newRow = tableResult.insertRow(tableResult.rows.length);
-  
-        const rowFirstname = newRow.insertCell(0);
-        rowFirstname.innerHTML = data.firstname;
-  
-        const rowLastname = newRow.insertCell(1);
-        rowLastname.innerHTML = data.lastname;
-  
-        const rowEmail = newRow.insertCell(2);
-        rowEmail.innerHTML = data.email;
-  
-        cleanInput();
-      })
-      .catch(error => {
-        console.error('Erro ao criar usuário', error);
-        alert('ERROR');
-      });
-  });
-  
-  function cleanInput() {
-    let firstname = document.getElementById('firstname');
-    const lastname = document.getElementById('lastname');
-    const email = document.getElementById('email');
-    firstname.value = '';
-    lastname.value = '';
-    email.value = '';
-  }
-  
+    .catch(error => {
+      console.error('Erro ao criar usuário', error);
+      alert('ERROR');
+    });
+});
+
+function cleanInput() {
+  let firstname = document.getElementById('firstname');
+  const lastname = document.getElementById('lastname');
+  const email = document.getElementById('email');
+  const cargo = document.getElementById('cargo');
+  const salario = document.getElementById('salario');
+  firstname.value = '';
+  lastname.value = '';
+  email.value = '';
+  cargo.value = ''; // Adicionado
+  salario.value = ''; // Adicionado
+}
